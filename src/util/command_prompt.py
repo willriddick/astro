1070fd3f9502752c9e4290d90d9634789a3f3936
willriddick <willriddick>
2025-01-15 20:10:40 -0500

    Added cursor movement and base levels

--- src/util/command_prompt.py
+++ src/util/command_prompt.py
@@ -12,6 +12,7 @@ class CommandPrompt:
         self.history: list[str] = []
         self.history_index = 0
         self.last_command: str = ''
+        self.cursor_index = 0
     
     def toggle(self):
         if self.enabled:
@@ -26,6 +27,7 @@ class CommandPrompt:
         self.enabled = False
         self.input = ''
         self.history_index = len(self.history)
+        self.cursor_index = 0
 
     def handle_command(self, command: str):
         self.history.append(command)
@@ -51,20 +53,32 @@ class CommandPrompt:
                     self.history_index = max(self.history_index - 1, 0)
                     if len(self.history) > 0:
                         self.input = self.history[self.history_index]
+                        self.cursor_index = len(self.input)
                 case pygame.K_DOWN:
                     self.history_index = min(self.history_index + 1, len(self.history))
                     if self.history_index == len(self.history):
                         self.input = ''
                     else:
                         self.input = self.history[self.history_index]
+                    self.cursor_index = len(self.input)
+                case pygame.K_LEFT:
+                    self.cursor_index = max(self.cursor_index - 1, 0)
+                case pygame.K_RIGHT:
+                    self.cursor_index = min(self.cursor_index + 1, len(self.input))
                 case pygame.K_BACKSPACE:
-                    self.input = self.input[:-1]
+                    if self.cursor_index > 0:
+                        self.input = self.input[:self.cursor_index - 1] + self.input[self.cursor_index:]
+                        self.cursor_index -= 1
                 case _:
-                    self.input += event.unicode
+                    if event.unicode.isprintable():
+                        self.input = self.input[:self.cursor_index] + event.unicode + self.input[self.cursor_index:]
+                        self.cursor_index += 1
 
-        # Toggle prompt with K_SLASH
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SLASH:
-            self.toggle()
+        # Enabled prompt with K_SLASH
+        if (not self.enabled 
+            and event.type == pygame.KEYDOWN 
+            and event.key == pygame.K_SLASH):
+            self.enable()
 
     def render(self, display: pygame.Surface):
         if self.enabled: 
@@ -74,7 +88,7 @@ class CommandPrompt:
                 color=RECT_COLOR, 
                 alpha=128
             )
-            cursor = '_' if int(time.time() * 2) % 2 == 0 else ' '
-            text_surf = Assets.FONT.render(f'/{self.input}{cursor}', antialias=False, color=(255, 255, 255))
+            text = f'/{self.input[:self.cursor_index]}_{self.input[self.cursor_index:]}'
+            text_surf = Assets.FONT.render(text, antialias=False, color=(255, 255, 255))
             display.blit(text_surf, (4, display.get_height() - 8))  
     
