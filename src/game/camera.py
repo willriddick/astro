73320ff0897666b1099d1f5237eba59abf1660e4
willriddick <willriddick>
2025-01-27 22:14:12 -0500

    Map fixes

--- src/game/camera.py
+++ src/game/camera.py
@@ -101,7 +101,7 @@ class Camera:
         """Set the camera's position to a target position."""
         self.pos = target_pos
     
-    def move_to(self, target_pos: pygame.Vector2, smoothing=20, factor=20) -> None:
+    def move_to(self, target_pos: pygame.Vector2, smoothing=10, factor=20) -> None:
         """Smoothly interpolate towards the target position using an exponential decay approach."""
         # Calculate dynamic smoothing based on the distance to the target
         distance_to_target = (target_pos - self.pos).length()
--- src/game/player/player.py
+++ src/game/player/player.py
@@ -29,7 +29,7 @@ class Player(PhysicsEntity):
     SLIDE_BUFFER = 165 # time after landing to allow slide
 
     WALL_JUMP_DURATION = 80 # time after wall jumping to push player away from wall
-    WALL_JUMP_SPEED = Vec2(125, 165)
+    WALL_JUMP_SPEED = Vec2(120, 160)
     WALL_JUMP_ACC = (8, 8)
     WALL_SLIDE_SPEED = 30
     WALL_SLIDE_GRAVITY = 180
