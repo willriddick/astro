31feba91474ff0bf039d53a3d3d99430599d2b56
willriddick <willriddick>
2025-02-19 17:44:20 -0500

    Move most logic from menu to page

--- src/menu/menu.py
+++ src/menu/menu.py
@@ -9,39 +9,25 @@ class Menu():
         self.pages = pages
         self.position = position
         self.draw_direction = draw_direction
+        self.movement_enabled = True
 
         self.current_page: Page = None
         self.page_index = 0
-        self.button_index = 0
+        self.change_page(self.page_index)
 
-        self.movement_enabled = True
-
-        self.change_page(0)
-    
     def change_page(self, page_index: int):
         self.page_index = page_index % len(self.pages)
         self.current_page = self.pages[self.page_index]
-        self.button_index = self.current_page.index
 
     def update(self):
         if self.movement_enabled:
             input_dir = inputs.get_dir(just_pressed=True).y
             if input_dir:
-                self.button_index = int((self.button_index + input_dir) % self.current_page.button_count)
+                self.current_page.move_index(input_dir)
                 assets.SOUNDS.play('blip')
 
         if inputs.get('select', just_pressed=True):
-            self.current_page.buttons[self.button_index].select()
+            self.current_page.selected_button.select()
     
     def render(self, display, _):
-        for index, button in enumerate(self.current_page.buttons):
-            button.update(hovered=index == self.button_index)
-            surface = button.get_surface()
-            y_pos = self.position.y + (index * button.SURFACE_SIZE.y)
-
-            if self.draw_direction == 1:  # top-to-bottom
-                y_pos = self.position.y + (index * button.SURFACE_SIZE.y)
-            else:  # bottom-to-top
-                y_pos = self.position.y - self.current_page.page_height + (index * button.SURFACE_SIZE.y)
-
-            display.blit(surface, (self.position.x, y_pos))
+        self.current_page.render(display, self.position, self.draw_direction)
--- src/menu/page.py
+++ src/menu/page.py
@@ -5,9 +5,29 @@ class Page:
         self.buttons = buttons
         self.button_count = len(self.buttons)
         self.default_index = default_index
-        self.index = default_index
+        self.button_index = default_index
+    
+    def move_index(self, input_dir: int):
+        self.button_index = int((self.button_index + input_dir) % self.button_count)
+    
+    @property
+    def selected_button(self) -> Button:
+        return self.buttons[self.button_index]
     
     @property
     def page_height(self) -> int:
         return self.button_count * self.buttons[0].SURFACE_SIZE.y
     
+    def render(self, display, position, draw_dir):
+        for index, button in enumerate(self.buttons):
+            button.update(hovered=index == self.button_index)
+            surface = button.get_surface()
+            y_offset = index * button.SURFACE_SIZE.y
+            y_pos = position.y + y_offset
+
+            if draw_dir == 1:  # top-to-bottom
+                y_pos = position.y + y_offset
+            else:  # bottom-to-top
+                y_pos = position.y - self.page_height + y_offset
+
+            display.blit(surface, (position.x, y_pos))
