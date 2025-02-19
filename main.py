5fc5f2830cd6824a75033a4ee428986df2af5c44
willriddick <willriddick>
2025-02-19 15:31:15 -0500

    Added flag for running editor

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import argparse
 import asyncio
 from src.game import Game
+from src.tilemap.editor import Editor
 
 def main():
-    game = Game()
-    asyncio.run(game.run())
+    parser = argparse.ArgumentParser(description='Run the game or the editor.')
+    parser.add_argument('-e', '--editor', action='store_true', help='Run the editor.')
+    args = parser.parse_args()
+
+    if args.editor:
+        Editor().run()
+    else:
+        game = Game()
+        asyncio.run(game.run())
 
 if __name__ == "__main__":
     main()
