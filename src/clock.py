b03ae20ae7e0201b420fd8fbc4d61d10eb3c7b11
willriddick <willriddick>
2025-02-21 00:00:07 -0500

    Switched to CLOCK style singleton

--- src/clock.py
+++ src/clock.py
@@ -1,31 +1,30 @@
 import pygame
-import src.settings as settings
 
 class Clock:
     """A global clock for our game."""
-    _clock = pygame.time.Clock()
-    _dt = 0
-
-    @classmethod
-    def update(cls):
-        """
-        Run every game tick.
-        
-        Updates delta time to `_clock.tick() / 1000` since `tick()` returns milliseconds.
-        """
-        cls._dt = cls._clock.tick() / 1000
-
-    @classmethod
-    def dt(cls) -> float:
-        """Returns the time in seconds since last frame."""
-        return cls._dt
     
-    @classmethod
-    def fps(cls) -> int:
+    def __init__(self):
+        self._clock = pygame.time.Clock()
+        self._dt = 0
+    
+    @property
+    def dt(self) -> float:
+        """Returns the time in seconds since the last frame."""
+        return self._dt
+    
+    @property 
+    def fps(self) -> int:
         """Returns the current frames per second as an int."""
-        return int(cls._clock.get_fps())
+        return int(self._clock.get_fps())
     
-    @classmethod
-    def ticks(cls) -> int:
-        """Returns times since pygame.init() was called in milliseconds."""
+    @property
+    def ticks(self) -> int:
+        """Returns time since pygame.init() was called in milliseconds."""
         return pygame.time.get_ticks()
+
+    def update(self):
+        """Updates delta time to `_clock.tick() / 1000`."""
+        self._dt = self._clock.tick() / 1000
+
+
+CLOCK = Clock()
